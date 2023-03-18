3b4e9b8afe9085f1f296b60706b11e8869027659
Rishabh Batra <rishabh.batra@klassifai.com>
2023-03-17 22:23:12 -0700

    Changing the latitude and longitude

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -31,6 +31,7 @@ def go(args):
     idx = df['price'].between(min_price, max_price)
     df = df[idx].copy()
 
+    # Changes to the longitude and latitude
     idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
     df = df[idx].copy()
 
